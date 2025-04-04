adf5b6f7aca99603c053a879d4ccf926653e6c27
Iankennethgacusan <ganadoian31@gmail.com>
2025-04-04 23:23:50 +0800

    Create Boolean.js

---
+++ LA1_GACUSAN_CS1A/Boolean.js
@@ -0,0 +1,8 @@
+// Declare variables
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let favNumb = 30;
+let add = favNumb + undefined;
+
+// Display the values of the variables
+console.log(isJsProgrammingLanguage + "\n" + isJsHard + "\n" + favNumb + "\n" + add);
