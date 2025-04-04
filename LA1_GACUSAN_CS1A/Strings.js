1c915f57ef210f88d1582e1ba2f4f7e3d65d4d42
Iankennethgacusan <ganadoian31@gmail.com>
2025-04-04 23:32:25 +0800

    Create Strings.js

---
+++ LA1_GACUSAN_CS1A/Strings.js
@@ -0,0 +1,21 @@
+// Define the favorite actor's name
+let favActorFirstName = "Michael";
+let favActorLastName = "Jackson";
+
+// Construct the full name
+let fullName = `${favActorFirstName} ${favActorLastName}`; //Using template literal
+
+// Convert the full name to uppercase
+let uppercaseFullName = fullName.toUpperCase();
+
+// Create a message using template literals
+let message = `My favorite actor is ${uppercaseFullName}. You are the best and one of my favorite movies is Karate Kid.`;
+
+
+// Display the results
+console.log(`First Name: ${favActorFirstName}`);
+console.log(`Last Name: ${favActorLastName}`);
+console.log(`Full Name: ${fullName}`);
+console.log(`Uppercase Full Name: ${uppercaseFullName}`);
+console.log(`Message: ${message}`);
+
