06b33fd3d583e4cbe5a09ebc7165820f31fba4d2
Iankennethgacusan <ganadoian31@gmail.com>
2025-04-04 23:30:32 +0800

    Create Variables.js

---
+++ LA1_GACUSAN_CS1A/Variables.js
@@ -0,0 +1,11 @@
+// Get user input (simulated here;  in a real application, you'd use prompt())
+let name = "Gacusan Ian Kenneth";
+let futureProfession = "Professional Programmer"; //More descriptive name
+let gender = "Male";
+let twitterHandle = "ganadoian31@gmail.com";
+
+// Display user information using template literals
+console.log(`Name: ${name}`);
+console.log(`Future Profession: ${futureProfession}`);
+console.log(`Gender: ${gender}`);
+console.log(`Twitter Handle: ${twitterHandle}`);
