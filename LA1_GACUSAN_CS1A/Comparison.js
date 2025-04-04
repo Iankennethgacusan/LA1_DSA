4fdb0afe1a733ab60df24c945449c97c7df2f8c4
Iankennethgacusan <ganadoian31@gmail.com>
2025-04-04 23:25:33 +0800

    Create Comparison.js

---
+++ LA1_GACUSAN_CS1A/Comparison.js
@@ -0,0 +1,24 @@
+// Declare favorite numbers
+let firstFavNumb = 30;
+let secondFavNumb = 19;
+
+// Perform comparisons and assign boolean results
+let greaterThan = firstFavNumb > secondFavNumb;
+let lessThan = firstFavNumb < secondFavNumb;
+let greaterThanOrEqual = firstFavNumb >= secondFavNumb;
+let lessThanOrEqual = firstFavNumb <= secondFavNumb;
+let strictEquality = firstFavNumb === secondFavNumb;
+let looseEquality = firstFavNumb == secondFavNumb;
+let strictInequality = firstFavNumb !== secondFavNumb;
+let looseInequality = firstFavNumb != secondFavNumb;
+
+// Output the comparison results
+console.log("Greater Than:", greaterThan);
+console.log("Less Than:", lessThan);
+console.log("Greater Than or Equal To:", greaterThanOrEqual);
+console.log("Less Than or Equal To:", lessThanOrEqual);
+console.log("Strict Equality:", strictEquality);
+console.log("Loose Equality:", looseEquality);
+console.log("Strict Inequality:", strictInequality);
+console.log("Loose Inequality:", looseInequality);
+
